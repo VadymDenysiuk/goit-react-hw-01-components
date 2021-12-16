211f4c096bab0af057cad9f9d2fb91e6696f688e
VadymDenysiuk <80970410371q@gmail.com>
2021-12-16 09:26:57 +0200

    add files

---
+++ src/components/Friends/FriendList.js
@@ -0,0 +1,33 @@
+import PropTypes from 'prop-types';
+import FriendListItem from './FriendListItem';
+import s from './Friend.module.css';
+
+function FriendList({ friends }) {
+  return (
+    <ul className={s.list}>
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <FriendListItem
+            key={id}
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+          />
+        );
+      })}
+    </ul>
+  );
+}
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      avatar: PropTypes.string,
+      name: PropTypes.string,
+      isOnline: PropTypes.bool,
+      id: PropTypes.number,
+    }),
+  ),
+};
+
+export default FriendList;
